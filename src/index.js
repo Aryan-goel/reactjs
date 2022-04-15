6d8b1b6fc324aa91c75b73bdd4f478d8385d2fe5
91639 <goel.aryan2406@gmail.com>
2022-04-16 03:07:11 +0530

    props

--- src/index.js
+++ src/index.js
@@ -1,26 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import style from "./index.css";
+import "./index.css";
 
-const title = "Ikigai";
-const author = "Hector gracia";
-const img =
-  "https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL906_SR906,600_.jpg";
+const firstBook = {
+  img: "https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL906_SR906,600_.jpg",
+  title: "Ikigai",
+  author: "Hector gracia",
+  price: "20$",
+};
+const secondBook = {
+  img: "https://images-eu.ssl-images-amazon.com/images/I/71g2ednj0JL._AC_UL906_SR906,600_.jpg",
+  title: "The Psychology of money",
+  author: "Morgan housel",
+  price: "35$",
+};
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
+      <Book
+        img={firstBook.img}
+        titile={firstBook.title}
+        author={firstBook.author}
+        price={firstBook.price}
+      />
+      <Book
+        img={secondBook.img}
+        titile={secondBook.title}
+        author={secondBook.author}
+        price={secondBook.price}
+      />
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
+  console.log(props);
   return (
     <article className="book">
-      <img src={img} alt="" />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
+      <img src={props.img} alt="" />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
+      <p>{props.job}</p>
+      <p>{props.titile}</p>
+      <p>{props.price}</p>
     </article>
   );
 };

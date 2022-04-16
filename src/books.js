ee2f7be34cfcb0e1662ccef0d470ebb3b51a79b3
91639 <goel.aryan2406@gmail.com>
2022-04-16 18:24:44 +0530

    using exports

---
+++ src/books.js
@@ -0,0 +1,23 @@
+export const books = [
+  {
+    id: 1,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL906_SR906,600_.jpg",
+    title: "Ikigai",
+    author: "Hector gracia",
+    price: "20$",
+  },
+  {
+    id: 2,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/71g2ednj0JL._AC_UL906_SR906,600_.jpg",
+    title: "The Psychology of money",
+    author: "Morgan housel",
+    price: "35$",
+  },
+  {
+    id: 3,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/51S7KOWir7L._AC_UL906_SR906,600_.jpg",
+    title: "Atomic Habits",
+    author: "James Clear",
+    price: "30$",
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,29 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-const books = [
-  {
-    id: 1,
-    img: "https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL906_SR906,600_.jpg",
-    title: "Ikigai",
-    author: "Hector gracia",
-    price: "20$",
-  },
-  {
-    id: 2,
-    img: "https://images-eu.ssl-images-amazon.com/images/I/71g2ednj0JL._AC_UL906_SR906,600_.jpg",
-    title: "The Psychology of money",
-    author: "Morgan housel",
-    price: "35$",
-  },
-  {
-    id: 3,
-    img: "https://images-eu.ssl-images-amazon.com/images/I/51S7KOWir7L._AC_UL906_SR906,600_.jpg",
-    title: "Atomic Habits",
-    author: "James Clear",
-    price: "30$",
-  },
-];
+import { books } from "./books";
 
 function BookList() {
   return (
@@ -44,7 +22,12 @@ const Book = ({ img, title, author, price }) => {
     console.log(author);
   };
   return (
-    <article className="book">
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log(title);
+      }}
+    >
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4 onClick={() => console.log(author)}>{author}</h4>

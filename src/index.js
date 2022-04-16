a462aa97dc711d3d4e18c5716c3bd2f4e7d527f9
91639 <goel.aryan2406@gmail.com>
2022-04-16 10:32:40 +0530

    lists

--- src/index.js
+++ src/index.js
@@ -1,48 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-
-const firstBook = {
-  img: "https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL906_SR906,600_.jpg",
-  title: "Ikigai",
-  author: "Hector gracia",
-  price: "20$",
-};
-const secondBook = {
-  img: "https://images-eu.ssl-images-amazon.com/images/I/71g2ednj0JL._AC_UL906_SR906,600_.jpg",
-  title: "The Psychology of money",
-  author: "Morgan housel",
-  price: "35$",
-};
-
+const books = [
+  {
+    img: "https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL906_SR906,600_.jpg",
+    title: "Ikigai",
+    author: "Hector gracia",
+    price: "20$",
+  },
+  {
+    img: "https://images-eu.ssl-images-amazon.com/images/I/71g2ednj0JL._AC_UL906_SR906,600_.jpg",
+    title: "The Psychology of money",
+    author: "Morgan housel",
+    price: "35$",
+  },
+];
+const names = ["aryan", "karan", "kabir"];
+const newName = names.map((name) => {
+  return <h1>{name}</h1>;
+  console.log(names);
+});
+console.log(newName);
 function BookList() {
-  return (
-    <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-        price={firstBook.price}
-      >
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium
-          reprehenderit quidem non tenetur expedita aliquam eius animi quibusdam
-          hic cum?
-        </p>
-      </Book>
-
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-        price={secondBook.price}
-      ></Book>
-    </section>
-  );
+  return <section className="booklist">{newName}</section>;
 }
 
 const Book = (props) => {
-  const { img, title, author, price, children } = props;
+  const { img, title, author, price } = props;
   console.log(props);
   return (
     <article className="book">
@@ -50,7 +34,6 @@ const Book = (props) => {
       <h1>{title}</h1>
       <h4>{author}</h4>
       <h4>{price}</h4>
-      {children}
     </article>
   );
 };

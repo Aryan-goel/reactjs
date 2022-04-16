e00cbad1d86d75bf990b8f6e4364c87bef8a5e9a
91639 <goel.aryan2406@gmail.com>
2022-04-16 13:33:55 +0530

    key props

--- src/index.js
+++ src/index.js
@@ -3,18 +3,21 @@ import ReactDOM from "react-dom";
 import "./index.css";
 const books = [
   {
+    id: 1,
     img: "https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL906_SR906,600_.jpg",
     title: "Ikigai",
     author: "Hector gracia",
     price: "20$",
   },
   {
+    id: 2,
     img: "https://images-eu.ssl-images-amazon.com/images/I/71g2ednj0JL._AC_UL906_SR906,600_.jpg",
     title: "The Psychology of money",
     author: "Morgan housel",
     price: "35$",
   },
   {
+    id: 3,
     img: "https://images-eu.ssl-images-amazon.com/images/I/51S7KOWir7L._AC_UL906_SR906,600_.jpg",
     title: "Atomic Habits",
     author: "James Clear",
@@ -26,8 +29,7 @@ function BookList() {
   return (
     <section className="booklist">
       {books.map((book) => {
-        const { img, title, author } = book;
-        return <Book book={book} />;
+        return <Book key={book.id} book={book} />;
       })}
     </section>
   );
